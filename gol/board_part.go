64713e21bfa879e5e9bc912f6dfeb064fffd80b2
kamil <wojcikakamil@gmail.com>
2022-05-13 11:33:27 +0200

    creating alternative board splitting method

--- gol/board_part.go
+++ gol/board_part.go
@@ -4,7 +4,6 @@ import (
 	"math"
 	"math/rand"
 	"os"
-	"sort"
 	"strconv"
 	"strings"
 )
@@ -46,7 +45,8 @@ type BoardPart struct {
 	// matrix is of dimensions Width+2 x Height+2 because of
 	// edges of map needed to calc next iteration
 	Board  [][]int8
-	PartNo uint32
+	StartW uint32
+	StartH uint32
 }
 
 func (b BoardPart) FPrint(iteration int, file *os.File) error {
@@ -149,97 +149,123 @@ func (b BoardPart) getNeighbors(x, y uint32) int8 {
 	return sum
 }
 
-// Split into sqrt(n)
-func (b BoardPart) Split(n uint32) []BoardPart {
-	nSqrt := uint32(math.Floor(math.Sqrt(float64(n)))) // split into grid nSqrt x nSqrt
-	extraSplits := n - nSqrt*nSqrt                     // how many times split part of grid into 2
+func (b BoardPart) Split(n uint32) ([]BoardPart, error) {
+	parts := make([]BoardPart, 1, n)
+	partsCounter := 1
+	partsIterator := 0
 
-	parts := make([]BoardPart, n)
-	partW, partH := b.Width/nSqrt, b.Height/nSqrt
-	extraSplitsCounter, partNo := extraSplits, uint32(0)
+	for i := uint32(0); i < n; i++ {
 
-	for yPart := uint32(0); yPart < nSqrt; yPart++ {
-		startY := partH * yPart
-
-		for xPart := uint32(0); xPart < nSqrt; xPart++ {
-			startX := partW * xPart
-
-			columns := make([][]int8, partW+2)
-
-			for i := uint32(0); i < partW+2; i++ {
-				columns[i] = b.Board[startX+i][startY : startY+partH+2]
-			}
-
-			parts[partNo] = BoardPart{
-				Width:  partW,
-				Height: partH,
-				Board:  columns,
-				PartNo: partNo,
-			}
-
-			if extraSplitsCounter > 0 {
-				partNo += 2
-				extraSplitsCounter--
-			} else {
-				partNo++
-			}
-		}
 	}
-
-	for i := uint32(0); i < extraSplits; i += 2 {
-		part := parts[i]
-		p1, p2 := part.splitInto2()
-		p1.PartNo = part.PartNo
-		p2.PartNo = part.PartNo + 1
-		parts[i] = p1
-		parts[i+1] = p2
-	}
-
-	return parts
 }
 
-func (b BoardPart) splitInto2() (BoardPart, BoardPart) {
-	width, height := b.Width/2, b.Height
-	b1Cols, b2Cols := make([][]int8, width+2), make([][]int8, width+2)
+func (b BoardPart) splitInto2(parts []BoardPart, partIndex int) {
+	board := parts[partIndex]
+	b1 := BoardPart{
+		Width:  uint32(math.Ceil(float64(board.Width) / 2)),
+		Height: uint32(math.Ceil(float64(board.Height) / 2)),
+		Board:  nil,
+		StartW: board.StartW,
+		StartH: board.StartH,
+	}
+	b1.Board = make([][]int8, b1.Height)
+	for y := uint32(0); y < b1.Height; y++ {
 
-	for x := uint32(0); x < width+2; x++ {
-		b1Cols[x] = b.Board[x]
-		b2Cols[x] = b.Board[x+width]
 	}
 
-	return BoardPart{
-			Width:  width,
-			Height: height,
-			Board:  b1Cols,
-			PartNo: 0,
-		}, BoardPart{
-			Width:  width,
-			Height: height,
-			Board:  b2Cols,
-			PartNo: 1,
-		}
 }
 
-func (b BoardPart) Merge(parts []BoardPart) BoardPart {
-	sort.Slice(parts, func(i, j int) bool {
-		return parts[i].PartNo < parts[j].PartNo
-	})
-
-	merged := emptyBoardPart(b.Width, b.Height)
-	mergedX, mergedY := uint32(0), uint32(0)
-
-	for _, part := range parts {
-		for x := uint32(1); x < part.Width+1; x++ {
-			for y := uint32(1); y < part.Height+1; y++ {
-				merged.Board[mergedX+x][mergedY+y] = part.Board[x][y]
-			}
-		}
-		mergedX += part.Width
-		if mergedX == b.Width {
-			mergedX = 0
-			mergedY += part.Height
-		}
-	}
-
-	return merged
-}
+//// Split into sqrt(n)
+//func (b BoardPart) Split(n uint32) []BoardPart {
+//	nSqrt := uint32(math.Floor(math.Sqrt(float64(n)))) // split into grid nSqrt x nSqrt
+//	extraSplits := n - nSqrt*nSqrt                     // how many times split part of grid into 2
+//
+//	parts := make([]BoardPart, n)
+//	partW, partH := b.Width/nSqrt, b.Height/nSqrt
+//	extraSplitsCounter, partNo := extraSplits, uint32(0)
+//
+//	for yPart := uint32(0); yPart < nSqrt; yPart++ {
+//		startY := partH * yPart
+//
+//		for xPart := uint32(0); xPart < nSqrt; xPart++ {
+//			startX := partW * xPart
+//
+//			columns := make([][]int8, partW+2)
+//
+//			for i := uint32(0); i < partW+2; i++ {
+//				columns[i] = b.Board[startX+i][startY : startY+partH+2]
+//			}
+//
+//			parts[partNo] = BoardPart{
+//				Width:  partW,
+//				Height: partH,
+//				Board:  columns,
+//				PartNo: partNo,
+//			}
+//
+//			if extraSplitsCounter > 0 {
+//				partNo += 2
+//				extraSplitsCounter--
+//			} else {
+//				partNo++
+//			}
+//		}
+//	}
+//
+//	for i := uint32(0); i < extraSplits; i += 2 {
+//		part := parts[i]
+//		p1, p2 := part.splitInto2()
+//		p1.PartNo = part.PartNo
+//		p2.PartNo = part.PartNo + 1
+//		parts[i] = p1
+//		parts[i+1] = p2
+//	}
+//
+//	return parts
+//}
+//
+//func (b BoardPart) splitInto2() (BoardPart, BoardPart) {
+//	width, height := b.Width/2, b.Height
+//	b1Cols, b2Cols := make([][]int8, width+2), make([][]int8, width+2)
+//
+//	for x := uint32(0); x < width+2; x++ {
+//		b1Cols[x] = b.Board[x]
+//		b2Cols[x] = b.Board[x+width]
+//	}
+//
+//	return BoardPart{
+//			Width:  width,
+//			Height: height,
+//			Board:  b1Cols,
+//			PartNo: 0,
+//		}, BoardPart{
+//			Width:  width,
+//			Height: height,
+//			Board:  b2Cols,
+//			PartNo: 1,
+//		}
+//}
+//
+//func (b BoardPart) Merge(parts []BoardPart) BoardPart {
+//	sort.Slice(parts, func(i, j int) bool {
+//		return parts[i].PartNo < parts[j].PartNo
+//	})
+//
+//	merged := emptyBoardPart(b.Width, b.Height)
+//	mergedX, mergedY := uint32(0), uint32(0)
+//
+//	for _, part := range parts {
+//		for x := uint32(1); x < part.Width+1; x++ {
+//			for y := uint32(1); y < part.Height+1; y++ {
+//				merged.Board[mergedX+x][mergedY+y] = part.Board[x][y]
+//			}
+//		}
+//		mergedX += part.Width
+//		if mergedX == b.Width {
+//			mergedX = 0
+//			mergedY += part.Height
+//		}
+//	}
+//
+//	return merged
+//}
